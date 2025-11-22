c7fb2a1641d48d9216ed1507764e858a8400fca5
Manoj Chowdary <manojchow72@gmail.com>
2025-11-22 16:00:01 +0530

    Cross origin added on login controller

--- src/main/java/com/whistleup/backend/controllers/LoginController.java
+++ src/main/java/com/whistleup/backend/controllers/LoginController.java
@@ -9,13 +9,11 @@ import lombok.extern.java.Log;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/whistleup/users")
+@CrossOrigin(origins = "*")
 public class LoginController {
 
     private final AuthenticationService authenticationService;
