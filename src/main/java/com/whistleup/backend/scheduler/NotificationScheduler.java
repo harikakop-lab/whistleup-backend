44557b82b204854c5da080f13a3f12f3bd757921
Manoj Chowdary <manojchow72@gmail.com>
2026-04-03 17:17:18 +0530

    Resolved conflicts

--- src/main/java/com/whistleup/backend/scheduler/NotificationScheduler.java
+++ src/main/java/com/whistleup/backend/scheduler/NotificationScheduler.java
@@ -54,22 +54,4 @@ public class NotificationScheduler {
             default -> "current";
         };
     }
-
-    private String getMonth(Integer maintenanceMonth) {
-        return switch (maintenanceMonth) {
-            case 1 -> "January";
-            case 2 -> "February";
-            case 3 -> "March";
-            case 4 -> "April";
-            case 5 -> "May";
-            case 6 -> "June";
-            case 7 -> "July";
-            case 8 -> "August";
-            case 9 -> "September";
-            case 10 -> "October";
-            case 11 -> "November";
-            case 12 -> "December";
-            default -> "current";
-        };
-    }
 }
